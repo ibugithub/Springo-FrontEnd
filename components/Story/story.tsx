001a5d1147223b270ecd324b3bbbf5aa9da3010f
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-18 06:08:51 +0600

    Add loading state to story page and fix delete story issue

--- components/Story/story.tsx
+++ components/Story/story.tsx
@@ -6,11 +6,19 @@ import { Story } from "../interface";
 import { BaseUrl } from "../utils/baseUrl";
 
 export const ShowStories = () => {
-  const [stories, setStories] = useState<Story[]>([]);
+  const [stories, setStories] = useState<Story[] | string>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const fetch = async () => {
     const url = `${BaseUrl}/api/stories/show_story/`
     const response = await axios.get(url);
-    setStories(response.data);
+    if (response.status === 200) {
+      setStories(response.data);
+      setIsLoading(false);
+    } else {
+      setStories('could not load stories');
+      setIsLoading(false);
+    }
+
   };
 
   useEffect(() => {
@@ -19,21 +27,27 @@ export const ShowStories = () => {
     };
     fetchStories();
   }, []);
-  console.log('the stories are ', stories)
   return (
     <div className="bg-white text-red-900 pl-10 pb-10">
-      <h2 className='mb-14 text-2xl text-red-600'>Page is under construction....</h2>
-      <div className="flex justify-center">
-        <div><a href="/uploadStory" className="text-green-600">Write Your Own Story</a></div>
-      </div>
-      <div>
-        {stories.map((story) => (
-          <div key={story.id}>
-            <span className="text-2xl ">{story.name}</span> by <span className="text-2xl text-green-400"> {story.author}</span>
-            <p>Description: {story.story}</p>
-          </div>
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center">
+          <div className="">Loading data...</div>
+        </div>
+      ) : (
+      <>
+        <div className="flex justify-center">
+          <div><a href="/uploadStory" className="text-green-600">Write Your Own Story</a></div>
+        </div>
+        <div>
+          {stories.map((story) => (
+            <div key={story.id}>
+              <span className="text-2xl ">{story.name}</span> by <span className="text-2xl text-green-400"> {story.author}</span>
+              <p>Description: {story.story}</p>
+            </div>
+          ))}
+        </div>
+      </>
+      )}
     </div>
   );
 };
