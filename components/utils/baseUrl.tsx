6a74525175464b80f2fcc747390a0eac3e9304db
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-24 09:45:29 +0600

    Change the base url

--- components/utils/baseUrl.tsx
+++ components/utils/baseUrl.tsx
@@ -1,3 +1,3 @@
-export const BaseUrl = "http://127.0.0.1:8000"
+// export const BaseUrl = "http://127.0.0.1:8000"
 
-// export const BaseUrl = "https://springo-backend.onrender.com"
+export const BaseUrl = "https://springo-backend.onrender.com"
